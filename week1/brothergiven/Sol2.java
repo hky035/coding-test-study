bd4d667fdec8d3e30d7f8c1df3b03e4164273054
brothergiven <dung3740@gmail.com>
2024-07-04 03:54:37 +0900

    last commit

--- week1/brothergiven/Sol2.java
+++ week1/brothergiven/Sol2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Sol2 : PROG 42860 조이스틱
  */
@@ -19,26 +21,36 @@ public class Sol2 {
      * 더 멀리 있는 A로 간다.
      * 그 다음은? 그 A에서 다시 검사해서 더 멀리있는 A로 이동
      * A가 없다면 그냥 순차탐색
-     * 2. 위로 갈지 오른쪽으로 갈지 결정
+     * 2. 위로 갈지 오른쪽으로 갈지 결정 
      * 2-a. ASCII로 비교하여서 더 가까운 방향으로 가면 됨
+     * 
+     * 1번이랑 2번이랑 완전 따로 계산할 수 있음.
+     * 2번은 그냥 하면 되고
+     * 1번은 어떻게?
+     * 모든 위치를 한 번씩은 가야하지만 "연속한 A" 는 갈 필요가 없음 (한 칸씩 이동하다가 마지막에 가면 된다)
+     * "연속한 A"
+     * 연속하지 않은 A에 대해서 어차피 연속하지 않으면 걔네는 지나쳐야함
+     * 따라서 연속한 A가 가장 큰 지점을 알아내야 한당!
+     * 연결요소 찾기로 하면 될라낭?
      */
     int N;
-    String name;
-    boolean marked[];
 
+    String name;
+    // ex. DDAAADAAC
     public int solution(String name) {
         int answer = 0;
         this.name = name;
         N = name.length();
-        marked = new boolean[N];
-        int cursor = 0; // 현재 커서의 위치
-        int nextA, count; // nextA : 다음 이동할 A
-        // 전부 mark 될 때까지 반복, 
-        while(allMarked()){
-            nextA = findNextA(cursor); // cursor부터 nextA까지 반복,  
-
-
+        for(int i = 0; i < N; i++){
+            answer += marking(i);
         }
+
+        int arr[] = findA(name); 
+        // 가
+
+
+
+
         return answer;
     }
 
@@ -51,36 +63,35 @@ public class Sol2 {
      * @return 조이스틱을 움직이기 위해 조작한 횟수
      */
     int marking(int cursor){
-        marked[cursor] = true;
         return Math.min(name.charAt(cursor) - 'A', 'Z' - name.charAt(cursor) + 1);
     }
 
-
-    int findNextA(int cursor) { // 다음 이동해야하는 A의 위치 return
-        int left = -1, right = -1;
-
-        for (int i = (cursor + 1) % N; i != cursor; i = (i + 1) % N) // 오른쪽으로 이동
-        {
-            if (name.charAt(i) == 'A') {
-                left = i;
-                break;
-            }
+    /**
+     * 
+     * @param name 이름
+     * @return 각 cursor 위치에 대해서 인접한 A의 개수 반환(오른쪽으로만 이동)
+     */
+    int[] findA(String name){ 
+        int[] list = new int[name.length()];
+        for(int i = 0; i < N; i++){
+            list[i] = countA(name, i);
         }
+        // 이배열에서 값이 가장 큰 놈을 고름
+        // 값이 가장 큰 놈이 2개 이상이면 0번 인덱스를 기준으로 왼쪽, 오른쪽 검사해서 더 가까운놈
 
-        for (int i = (cursor == 0) ? N - 1 : cursor - 1; i != cursor; i = (i == 0) ? N - 1 : i - 1) {
-            if (name.charAt(i) == 'A') {
-                right = i;
-                break;
-            }
-        }
-
-        return Math.max(left, right); // name 안에서 더 찾을 A 없다면
+        return list;
     }
 
-    boolean allMarked(){
-        for(boolean b : marked)
-            if(!b) return false; // mark 안된 게 있다면
-        return true;
+    int countA(String name, int idx){
+        if(name.charAt(idx) != 'A') return 0;
+        else return 1 + countA(name, (idx + 1) % N);
     }
 
+    public static void main(String[] args) {
+        
+        String name = "ADDAADDCDAA";
+        System.out.println(findA(name, name.length()));
+
+
+    }
 }
