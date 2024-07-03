7b6d441494a25c3d1a0369360823631d8dab90e5
minseok <minsuk6803@gmail.com>
2024-07-04 07:26:21 +0900

    49191 문제 풀이 완료

---
+++ week1/minseok/prog_49191.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    LinkedList<LinkedList<Integer>> win = new LinkedList<>();//승자 정보
+    LinkedList<LinkedList<Integer>> lose = new LinkedList<>();//패자 정보
+    Set<Integer> visit = new HashSet<>();//방문처리를 위한 HashSet
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        for(int i =0 ;i<=n;i++){
+            LinkedList<Integer>t1 = new LinkedList<>();
+            LinkedList<Integer>t2 = new LinkedList<>();
+            win.add(t1);
+            lose.add(t2);
+        }
+        for(int i =0 ;i<results.length;i++){
+            win.get(results[i][0]).add(results[i][1]);//승자 정보 하나씩 저장
+            lose.get(results[i][1]).add(results[i][0]);//패자 정보 하나씩 저장
+        }
+        for(int i = 1; i<=n;i++){
+            visit.add(i);//각 노드 삽입
+            checkWin(i);//해당 노드가 이긴 노드 탐색
+            checkLose(i);//해당 노드가 진 노드 탐색
+            if(visit.size()==n)//해당 노드의 정보가 선수 수만큼 있으면 순위 계산 가능
+                answer++;
+            visit.clear();//방문처리 초기화
+        }
+        return answer;
+    }
+    public void checkWin(int node){
+        for(int i = 0 ;i<win.get(node).size();i++){
+            int next = win.get(node).get(i);
+            if(!visit.contains(next)){//무의미한 재귀를 막기 위한 조건문, 이미 탐색했던 건 굳이 더 깊이 안 들어가도 됨
+                 visit.add(next);//node가 이긴 놈들을 방문처리함
+                 checkWin(next);//그 놈에 대해 다시 파고듬
+             }
+        }
+    }
+    public void checkLose(int node){
+        for(int i = 0 ;i<lose.get(node).size();i++){
+             int next = lose.get(node).get(i);
+             if(!visit.contains(next)){
+                 visit.add(next);
+                 checkLose(next);
+             }
+        }
+    }
+}
